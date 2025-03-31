e6f79e249b6557b2014bcb653d69b5c4c33e47ca
Parveen <parveen708237@gmail.com>
2025-03-31 23:42:13 +0530

    added log interfiace and custom middleware

--- OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -9,16 +9,18 @@ namespace OdeToFood.Pages.Restaurants
     {
         private readonly IConfiguration config;  //  Injects the configuration service to access settings from appsettings.json.
         private readonly IRestaurantData restaurantData;
+        private readonly ILogger<ListModel> logger;
 
         public string Message { get; set; }  // Property to store the message
         public IEnumerable<Restaurant> Restaurants { get; set; }
 
         [BindProperty(SupportsGet = true)]//asp.net method when this class instatiated this method always search for this parameter in Http request and populate itself automatically.
         public string SearchTerm { get; set; }
-        public ListModel(IConfiguration config, IRestaurantData restaurantData) // Constructor with Dependency Injection
+        public ListModel(IConfiguration config, IRestaurantData restaurantData,ILogger<ListModel> logger) // Constructor with Dependency Injection
         {
             this.config = config;
             this.restaurantData = restaurantData;
+            this.logger = logger;
         }
 
         //public void OnGet(string searchTerm)  // This method is called when the page is accessed and in paras this is model binding.
@@ -26,6 +28,7 @@ namespace OdeToFood.Pages.Restaurants
         //Model Binding in ASP.NET Core automatically maps HTTP request data (like query strings, form inputs, and
         //route values) to C# properties or parameters. This eliminates the need for manual request parsing.
         {
+            logger.LogError("Executing ListModel"); 
             //Message = config["Message"]; // Reading "Message" from appsettings.json
             //Restaurants = restaurantData.GetAll();
             Restaurants = restaurantData.GetRestaurantByName(SearchTerm);
--- OdeToFood/OdeToFood/Program.cs
+++ OdeToFood/OdeToFood/Program.cs
@@ -4,6 +4,7 @@ using OdeToFood.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Logging.AddConsole();
 builder.Services.AddRazorPages();
 builder.Services.AddControllers();
 builder.Services.AddDbContextPool<OdeToFoodDbContext>(options =>
@@ -22,7 +23,46 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+/*
+   In ASP.NET Core, middleware components handle HTTP requests and responses in a pipeline. Each middleware 
+  can process, modify, or terminate requests before passing them to the next middleware.
+ 
+ * var builder = WebApplication.CreateBuilder(args);
+    var app = builder.Build();
+    app.UseDeveloperExceptionPage(); // 1. Handles exceptions (only in development)
+    app.UseHttpsRedirection(); // 2. Redirects HTTP to HTTPS
+    app.UseStaticFiles(); // 3. Serves static files (CSS, JS, images)
+    app.UseRouting(); // 4. Enables routing system
+    app.UseAuthorization(); // 5. Enforces authorization policies
+    app.MapControllers(); // 6. Maps controllers (API endpoints)
+    app.Run();
+*/
+
+app.Use(SayHelloMiddleWare);//custom middleware
+
+
+/*
+ In ASP.NET Core, a RequestDelegate is a function that processes HTTP requests and can either:
+
+Handle the request itself (e.g., returning a response).
+
+Pass it to the next middleware in the pipeline.*/
+RequestDelegate SayHelloMiddleWare(RequestDelegate next)
+{
+    return async ctx =>
+    {
+        if (ctx.Request.Path.StartsWithSegments("/hello"))
+        {
+            await ctx.Response.WriteAsync("Hello World");
+        }
+        else
+        {
+            await next(ctx);
+        }
+    };
+}
+
+app.UseHttpsRedirection();//middleware to redirect http request to https
 app.UseStaticFiles();
 
 app.UseRouting();
